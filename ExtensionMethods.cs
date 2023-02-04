402a7b1d27dffdb26ff6f2d4e9097efdb05cb056
keenst <leo.siven@icloud.com>
2023-02-04 10:59:32 +0100

    Added proper mod method

---
+++ ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace astral_assault;
+
+public static class ExtensionMethods
+{
+    public static int Mod(this int x, int y)
+    {
+        return (Math.Abs(x * y) + x) % y;
+    }
+}
