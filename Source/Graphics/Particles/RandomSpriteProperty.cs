7ed245fbb576734b563e20e6714f87a391829680
keenst <leo.siven@icloud.com>
2023-03-21 16:48:05 +0100

    Fixed random sprite property

--- Source/Graphics/Particles/RandomSpriteProperty.cs
+++ Source/Graphics/Particles/RandomSpriteProperty.cs
@@ -4,11 +4,14 @@ namespace AstralAssault;
 
 public struct RandomSpriteProperty : IParticleProperty
 {
-    public int SpriteIndex { get; }
-    
+    private readonly Random _rnd = new();
+    private readonly int _rangeStart;
+    private readonly int _rangeEnd;
+    public int SpriteIndex => _rnd.Next(_rangeStart, _rangeEnd + 1);
+
     public RandomSpriteProperty(int rangeStart, int rangeEnd)
     {
-        Random rnd = new();
-        SpriteIndex = rnd.Next(rangeStart, rangeEnd + 1);
+        _rangeStart = rangeStart;
+        _rangeEnd = rangeEnd;
     }
 }
