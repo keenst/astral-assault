df66118a7cb3af890a16855cd7a521144af1e8d4
keenst <leo.siven@icloud.com>
2023-04-17 22:50:11 +0200

    Delayed the restart button

--- Source/Game/GameState/GameOverState.cs
+++ Source/Game/GameState/GameOverState.cs
@@ -10,10 +10,12 @@ public class GameOverState : GameState, IKeyboardPressedEventListener
 {
     private Texture2D _gameOverText;
     private Texture2D _restartPrompt;
+    private long _timeEntered;
     
     public GameOverState(Game1 root) : base(root)
     {
         InputEventSource.KeyboardPressedEvent += OnKeyboardPressedEvent;
+        _timeEntered = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
     }
     
     public override List<DrawTask> GetDrawTasks()
@@ -26,6 +28,8 @@ public class GameOverState : GameState, IKeyboardPressedEventListener
             (float)Math.Round(Game1.TargetWidth / 2D),
             (float)Math.Round(Game1.TargetHeight / 2D));
 
+        List<DrawTask> drawTasks = new();
+        
         DrawTask gameOverText = new(
             _gameOverText, 
             textPosition, 
@@ -33,23 +37,38 @@ public class GameOverState : GameState, IKeyboardPressedEventListener
             LayerDepth.HUD, 
             new List<IDrawTaskEffect>());
 
-        DrawTask restartPrompt = new(
-            _restartPrompt,
-            promptPosition,
-            0,
-            LayerDepth.HUD,
-            new List<IDrawTaskEffect>());
+        drawTasks.Add(gameOverText);
         
+        long timeNow = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        if (timeNow - _timeEntered > 1000)
+        {
+            DrawTask restartPrompt = new(
+                _restartPrompt,
+                promptPosition,
+                0,
+                LayerDepth.HUD,
+                new List<IDrawTaskEffect>());
+            
+            drawTasks.Add(restartPrompt);
+        }
+
         string scoreText = $"Score: {Root.Score}";
         Color textColor = Palette.GetColor(Palette.Colors.Grey9);
         int textX = 240 - scoreText.Length * 4;
         List<DrawTask> scoreTasks = scoreText.CreateDrawTasks(new Vector2(textX, 150), textColor, LayerDepth.HUD);
+        drawTasks.AddRange(scoreTasks);
 
-        return new List<DrawTask> { gameOverText, restartPrompt }.Concat(scoreTasks).ToList();
+        return drawTasks;
     }
 
     public void OnKeyboardPressedEvent(object sender, KeyboardEventArgs e)
     {
+        long timeNow = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        if (timeNow - _timeEntered < 300)
+        {
+            return;
+        }
+        
         Root.GameStateMachine.ChangeState(new GameplayState(Root));
     }
 
