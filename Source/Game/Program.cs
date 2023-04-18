b38115e5b23597ae983531aaa3df7049df4172fa
keenst <leo.siven@icloud.com>
2023-04-18 20:24:50 +0200

    Added logging in case it doesn't launch properly

--- Source/Game/Program.cs
+++ Source/Game/Program.cs
@@ -1,4 +1,11 @@
 ﻿using AstralAssault;
 
-using Game1 game = new();
-game.Run();
+try
+{
+    using Game1 game = new();
+    game.Run();
+}
+catch(System.Exception e)
+{
+    System.IO.File.WriteAllText("log.txt", e.ToString());
+}
