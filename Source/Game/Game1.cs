46bacf29a707230fd07231a74e068e02b0dd8843
keenst <leo.siven@icloud.com>
2023-05-04 16:52:16 +0200

    Started implementing pause menu

--- Source/Game/Game1.cs
+++ Source/Game/Game1.cs
@@ -96,18 +96,6 @@ public class Game1 : Game
 
     protected override void Update(GameTime gameTime)
     {
-        if (Keyboard.GetState().IsKeyDown(Keys.Escape) && !_prevKeyState.IsKeyDown(Keys.Escape))
-        {
-            if (GameStateMachine.CurrentState is not MainMenuState)
-            {
-                GameStateMachine.ChangeState(new MainMenuState(this));
-            }
-            else
-            {
-                Exit();
-            }
-        }
-
         if (Keyboard.GetState().IsKeyDown(Keys.F3) && !_prevKeyState.IsKeyDown(Keys.F3))
             ShowDebug = !ShowDebug;
 
--- Source/Game/GameState/GameplayState.cs
+++ Source/Game/GameState/GameplayState.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AstralAssault.Source.Menu;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace AstralAssault;
 
-public class GameplayState : GameState
+public class GameplayState : GameState, IKeyboardPressedEventListener
 {
     public readonly List<Entity> Entities;
     public readonly CollisionSystem CollisionSystem = new();
     public WaveController WaveController;
     
     public Player Player => (Player) Entities.Find(entity => entity is Player);
+    
+    private readonly MenuController _pauseMenuController;
+    private bool _isPaused;
 
     public GameplayState(Game1 root) : base(root)
     {
         Entities = new List<Entity>();
         WaveController = new WaveController(this, Root);
+        
+        string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        string json = File.ReadAllText(path + "/Content/Menus/Pause.json");
+
+        Menu pauseMenu = Menu.Parse(Root, json);
+        _pauseMenuController = new MenuController(pauseMenu, this);
+        _pauseMenuController.Close();
     }
 
     public override List<DrawTask> GetDrawTasks()
@@ -29,7 +43,8 @@ public class GameplayState : GameState
         }
 
         drawTasks.AddRange(WaveController.GetDrawTasks());
-
+        drawTasks.AddRange(_pauseMenuController.GetDrawTasks());
+        
         if (!Root.ShowDebug) return drawTasks;
         
         foreach (Collider collider in CollisionSystem.Colliders)
@@ -59,13 +74,27 @@ public class GameplayState : GameState
 
     public override void Enter()
     {
+        InputEventSource.KeyboardPressedEvent += OnKeyboardPressedEvent;
+        
         Entities.Add(new Player(this, new Vector2(Game1.TargetWidth / 2F, Game1.TargetHeight / 2F)));
         Entities.Add(new Crosshair(this));
     }
 
     public override void Exit()
     {
+        InputEventSource.KeyboardPressedEvent -= OnKeyboardPressedEvent;
+        
         WaveController.StopListening();
         while (Entities.Count > 0) Entities[0].Destroy();
+    }
+
+    public void OnKeyboardPressedEvent(object sender, KeyboardEventArgs e)
+    {
+        if (e.Keys.All(key => key != Keys.Escape)) return;
+        
+        _isPaused = !_isPaused;
+        
+        if (_isPaused) _pauseMenuController.Open();
+        else _pauseMenuController.Close();
     }
 }
